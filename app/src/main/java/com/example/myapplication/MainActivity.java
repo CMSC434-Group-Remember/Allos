a5602ce75c630e8502f2faeb283225d61c924527
CS436finalProject <1652765684@qq.com>
2019-12-08 20:35:03 -0500

    update UI

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -17,35 +17,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button add_contact = (Button) findViewById(R.id.btnAdd_Contact);
-        add_contact.setOnClickListener(new OnClickListener() {
-
-            // Call startExplicitActivation() when pressed
-            @Override
-            public void onClick(View v) {
-
-                btnAdd_Contact_onClick();
-
-            }
-        });
-    }
-
-    public void btnAdd_Contact_onClick() {
-
-        Intent intent = new Intent(ContactsContract.Intents.Insert.ACTION);
-        intent.setType(ContactsContract.RawContacts.CONTENT_TYPE);
-
-        EditText mEmailAddress = (EditText) findViewById(R.id.txtEmail);
-        EditText mPhoneNumber = (EditText) findViewById(R.id.txtTelephone);
-
-        intent.putExtra(ContactsContract.Intents.Insert.EMAIL, mEmailAddress.getText())
-                .putExtra(ContactsContract.Intents.Insert.EMAIL_TYPE, ContactsContract.CommonDataKinds.Email.TYPE_WORK)
-                .putExtra(ContactsContract.Intents.Insert.PHONE, mPhoneNumber.getText())
-
-                .putExtra(ContactsContract.Intents.Insert.PHONE_TYPE, ContactsContract.CommonDataKinds.Phone.TYPE_WORK);
-
-
-        startActivity(intent);
 
     }
+
+
 }
 
